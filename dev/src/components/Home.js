90b4d108d9df8f2809fb69a987f72542acbe328d
Kresnata <kresnata@majoo.id>
2021-05-03 06:19:27 +0800

    second

--- dev/src/components/Home.js
+++ dev/src/components/Home.js
@@ -1,88 +1,17 @@
-import React, { useEffect, useState, useRef, Component, lazy, Suspense } from 'react';
-// import Movie from '../components/Movie';
+import React, { useEffect, useState, useRef, lazy, Suspense } from 'react';
 import { featureMov, SEARCHAPI, fetchMovieByYear } from '../api/index';
 import DatePicker from 'react-datepicker';
 import moment from 'moment'
 import "react-datepicker/dist/react-datepicker.css";
+import { Card } from 'antd';
 
 const Movie = lazy(() => import('../components/Movie'));
 
-const Auto = () => {
-  const [display, setDisplay] = useState(false);
-  const [options, setOptions] = useState([]);
-  const [search, setSearch] = useState("");
-  const wrapperRef = useRef(null);
-
-  useEffect(() => {
-    const movies = [];
-    const promises = new Array(10)
-      .fill()
-      .map((v, i) => fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d212dc1bfc2d8009f736f68f2e71938f&page=1${i + 1}`));
-    Promise.all(promises).then(moviesArr => {
-      debugger;
-      return moviesArr.map(value =>
-        value
-          .json()
-          .then(({ data }) =>
-            movies.push({ data })
-          )
-      );
-    });
-    debugger;
-    setOptions(movies);
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      window.removeEventListener("mousedown", handleClickOutside);
-    };
-  });
-
-  const handleClickOutside = event => {
-    const { current: wrap } = wrapperRef;
-    if (wrap && !wrap.contains(event.target)) {
-      setDisplay(false);
-    }
-  };
-
-  const updateMovies = poke => {
-    setSearch(poke);
-    setDisplay(false);
-  };
-
-  return (
-    <div ref={wrapperRef} className="flex-container flex-column pos-rel">
-      <input
-        id="auto"
-        onClick={() => setDisplay(!display)}
-        placeholder="Type to search"
-        value={search}
-        onChange={event => setSearch(event.target.value)}
-      />
-      {display && (
-        <div className="autoContainer">
-          {options
-            .filter(({ title }) => title.indexOf(search.toLowerCase()) > -1)
-            .map((value, i) => {
-              return (
-                <div
-                  onClick={() => updateMovies(value.title)}
-                  className="option"
-                  key={i}
-                  tabIndex="0"
-                >
-                </div>
-              );
-            })}
-        </div>
-      )}
-    </div>
-  );
-};
-
 function App() {
   const [ movies, setMovies ] = useState([]);
+  const [ moviesMatch, setMoviesMatch ] = useState([]);
+  const wrapperRef = useRef(null);
+  const [display, setDisplay] = useState(false);
   const [ searchTerm, setSearchTerm ] = useState('');
   const [ yearTerm, setYearTerm] = useState(new Date());
 
@@ -93,6 +22,20 @@ function App() {
         setMovies(data.results);
       })
   }, [])
+
+  const handleClickOutside = event => {
+        const { current: wrap } = wrapperRef;
+        if (wrap && !wrap.contains(event.target)) {
+          setDisplay(false);
+        }
+      };
+
+      useEffect(() => {
+            window.addEventListener("mousedown", handleClickOutside);
+            return () => {
+              window.removeEventListener("mousedown", handleClickOutside);
+            };
+          });
 
   const handleSearch = async (e) => {
     const val = e.target.value;
@@ -131,18 +74,30 @@ function App() {
 
   }
 
+  const searchMovies = (val) => {
+    let matches = movies.filter((mov) => {
+      const regex = new RegExp(`${val}`, "gi");
+      return mov.title.match(regex) || mov.original_title.match(regex);
+    })
+    setMoviesMatch(matches);
+  };
+
   return (
     <div className="container" style={{ maxWidth: '100%' }}>
-       <header>
+       <header style={{ psotition: 'absolute' }}>
          <ul className="list-inline">
            <li className="list-inline-item ml-2">
-            <input
-              className="search"
-              type="text"
-              placeholder="search..."
-              value={searchTerm}
-              onChange={handleSearch}
-            />
+           <div ref={wrapperRef} className="flex-container flex-column pos-rel">
+              <input
+                className="search"
+                type="text"
+                placeholder="search..."
+                onClick={() => setDisplay(!display)}
+                // value={searchTerm}
+                // onChange={handleSearch}
+                onChange={(e) => searchMovies(e.target.value)}
+              />
+            </div>
            </li>
            <li className="list-inline-item">
             <DatePicker
@@ -152,13 +107,18 @@ function App() {
               showYearPicker
               dateFormat="yyyy"
               yearItemNumber={9}
+              className="search"
             />
            </li>
-           {/* <li className="list-inline-item">
-             <Auto />
-           </li> */}
          </ul>
       </header>
+      { moviesMatch && display && moviesMatch.map((x, i) => (
+                <div className="wrapCard">
+                    <Card style={{ width: '50%', position: 'absolute' }} title={`Film: ${x.title}`} >
+                      {x.title}
+                    </Card>
+                </div>
+              ))}
       <div className="movie-container">
       <Suspense fallback={<div>Loading</div>}>
       {movies.length > 0 && movies.map((mov) => 
